281b4bdcdf1c709050121c06f2e8689e9a240a20
Sergey <skilinlive@mail.ru>
2022-09-23 21:39:33 +0300

    proverka

--- resources/views/main/index.blade.php
+++ resources/views/main/index.blade.php
@@ -1,5 +1,6 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
 <link href="{{ asset('assets/pages/mainCss/main.css') }}" rel="stylesheet" type="text/css">
+<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 
 {{-- <div style="line-height: 23px;">Каталог</div> --}}
 
