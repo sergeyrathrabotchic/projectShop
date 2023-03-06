85a2867c5b3ba89f5cf06193d75b4aa1fd655da0
Sergey <skilinlive@mail.ru>
2023-03-06 22:39:26 +0300

    сдеалл вывод цены и описания

--- resources/views/ceramics/index.blade.php
+++ resources/views/ceramics/index.blade.php
@@ -143,9 +143,9 @@
 
 
         <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
+          <h5 class="card-title">{{$ceramic->price}}</h5>
+          <p class="card-text">{{$ceramic->description}}</p>
+          <!--<a href="#" class="btn btn-primary">Go somewhere</a>-->
         </div>
       </div>
     </div>
