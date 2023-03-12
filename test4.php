60f66b876f00ee140f5cb194ae45b6d683a3e945
Sergey <skilinlive@mail.ru>
2023-03-12 21:55:21 +0300

    доступы

--- test4.php
+++ test4.php
@@ -1,10 +1,77 @@
-sergeyrathrabotchic    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    1997SergeySemkin
+sergeyrathrabotchic    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    1997SergeySemkin
 
 izoS9tjW9H1Uv7nG
 
-/www/angelisolo.ru/projectShop
+Логин: angelikawm@yandex.ru
+Пароль: UZf3n_fi
 
+www/angelisolo.ru
 ssh u1685020@37.140.192.240
 
 ssh u1685020@37.140.192.240
+
+ssh u1685020@37.140.192.240
+пароль: 8bwQ43vP2pSCJ2Yh
+
+php artisan storage:link
+
+Доступ к FTP
+Логин: u1685020
+
+Пароль: D1pK0XpVQT2tw042
+
+IP-адрес сервера: 37.140.192.240
+
+
+
+
+
+
+
+
+
+
+
+
+Логин: u1863929
+Пароль: 4kwh82DqZUipJcm9
+IP-адрес сервера: 31.31.196.193
+
+
+
+ATBBHqAyYZ2bZRE37ckZVa5eS7m22BEBBDF0
+
+www/testproject.fun
+www/testprojectrus.fun
+
+ssh u1863929@31.31.196.193
+
+
+Доступ к MySQL
+Логин: u1863929_default
+
+Пароль: 4kwh82DqZUipJcm9
+
+Имя базы: u1863929_default
+
+Host: localhost
+
+
+
+
+
+
+Доступ к FTP
+Логин: u1864313
+
+Пароль: mFS4t788A2f5RJ2c
+
+IP-адрес сервера: 31.31.196.224
+
+ ssh u1864313@31.31.196.224
+
+cd www/testprojectcrm.fun
+cd www/testsitefanlucky.site
+cd www/testprojectruscrm.fun
+ATBBHqAyYZ2bZRE37ckZVa5eS7m22BEBBDF0
 
