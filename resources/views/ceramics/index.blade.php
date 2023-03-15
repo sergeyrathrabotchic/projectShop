668f58fd13fb3273456a53dd6a892fd4847999ee
Sergey <skilinlive@mail.ru>
2023-03-15 20:25:54 +0300

    фикс

--- resources/views/ceramics/index.blade.php
+++ resources/views/ceramics/index.blade.php
@@ -114,7 +114,7 @@
         
 
 
-<div id="carouselExampleControls {{$k}}" class="carousel slide" data-bs-ride="carousel">
+<div id="carouselExampleControls{{$k}}" class="carousel slide" data-bs-ride="carousel">
   <div class="carousel-inner">
     
     @php 
@@ -132,11 +132,11 @@
               @endif
         @endfor
     
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls {{$k}}" data-bs-slide="prev">
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls{{$k}}" data-bs-slide="prev">
           <span class="carousel-control-prev-icon" aria-hidden="true"></span>
           <span class="visually-hidden">Previous</span>
         </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls {{$k}}" data-bs-slide="next">
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls{{$k}}" data-bs-slide="next">
           <span class="carousel-control-next-icon" aria-hidden="true"></span>
           <span class="visually-hidden">Next</span>
         </button>
