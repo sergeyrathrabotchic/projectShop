9877ee485c834abc9f5f617b92765fe5997d105f
Sergey <skilinlive@mail.ru>
2023-03-29 22:23:22 +0300

    test

--- resources/views/admin/ceramics/create.blade.php
+++ resources/views/admin/ceramics/create.blade.php
@@ -14,13 +14,13 @@
      <button style="margin-right: 10px" id="fixImage" class="btn btn-success" type=button >Фиксация элемента</button>
      <a href="/storage/image/electroplating/_n64062f589e54f.jpeg" class="btn btn-success" download="FileName.png">Загрузка изображения</a>
   </div>
-  <div style="width: 100%;height: 400px;">
+  {{-- <div style="width: 100%;height: 400px;">
   <img id="image1" style="width: 100%;height: 400px;" src="/storage/image/electroplating/_n64062f589e54f.jpeg" alt="">
-</div>
+</div> --}}
   </div><div style="display: none" id="boxImage1">
     {{-- <img id="image1" style="width: 100%" src="/storage/image/electroplating/_n64062f589e54f.jpeg" alt=""> --}}
   <div style="width: 45%;margein-left:10px;height: 400px;"> 
-    {{-- <img id="image1" style="width: 100%" src="" alt=""> --}}
+    <img id="image1" style="width: 100%" src="" alt="">
   </div>
   <div style="width: 45%;height: 400px;">
     <img id="upload1" style="width: 100%" src="" alt="">
