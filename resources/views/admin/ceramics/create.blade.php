5e287d46a2c5e422b385bd6c78ff75432075651e
Sergey <skilinlive@mail.ru>
2023-03-29 19:55:07 +0300

    test

--- resources/views/admin/ceramics/create.blade.php
+++ resources/views/admin/ceramics/create.blade.php
@@ -16,9 +16,9 @@
   </div>
  
   <div style="display: none" id="boxImage1">
-    {{-- <img id="image1" style="width: 100%" src="/storage/image/electroplating/_n64062f589e54f.jpeg" alt=""> --}}
+    <img id="image1" style="width: 100%" src="/storage/image/electroplating/_n64062f589e54f.jpeg" alt="">
   <div style="width: 45%;margein-left:10px;height: 400px;"> 
-    <img id="image1" style="width: 100%" src="" alt="">
+    {{-- <img id="image1" style="width: 100%" src="" alt=""> --}}
   </div>
   <div style="width: 45%;height: 400px;">
     <img id="upload1" style="width: 100%" src="" alt="">
@@ -85,15 +85,71 @@
       <script src="{{ asset('js/croppie/croppie.min.js') }}"></script>
       <script type="text/javascript">
       //var croppie; 
-      var croppie2;
-      var image;
 
-      function test() {
-        // let boxImage1 = document.querySelector('#boxImage1');
-        // boxImage1.childNodes[1].remove()
+
+
+
+
+      // var croppie2;
+      // var image;
+
+      // function test() {
+      //   // let boxImage1 = document.querySelector('#boxImage1');
+      //   // boxImage1.childNodes[1].remove()
         
-        // setTimeout(test2, 2000);
-        const croppie = new Croppie(document.querySelector('#image1'), {
+      //   // setTimeout(test2, 2000);
+      //   const croppie = new Croppie(document.querySelector('#image1'), {
+      //     viewport: {
+      //           width: 150,
+      //           height: 200
+      //       }
+      //   });
+
+
+      // let fixImage = document.querySelector('#fixImage');
+
+      
+      // fixImage.addEventListener('click', function(){
+      //   //alert("work");
+      //   alert(croppie.getCroppedCanvas().toDataURL("image/png"));
+      //   var croppieImage = croppie.getCroppedCanvas().toDataURL("image/png");
+
+      //   alert(croppieImage);
+
+      //   //let upload1 = document.querySelector('#upload1');
+
+      //   //upload1.src = croppieImage;
+
+      // });
+        
+      // }
+      // function myFunction() {
+
+      //   // croppie.destroy();
+      //   // croppie = 0;
+      
+      // let boxImage1 = document.querySelector('#boxImage1');
+      // boxImage1.style = "width: 100%;height: 400px;margin-bottom: 50px;";
+      // var file = document.getElementById('file').files[0];
+      // var reader  = new FileReader();
+      // // it's onload event and you forgot (parameters)
+      // reader.onload = function(e)  {
+      //     //var image = document.createElement("img");
+      //     image = document.querySelector('#image1');
+      //     // the result image data
+      //     image.src = e.target.result;
+      //     //document.body.appendChild(image);
+      // }
+      // // you have to declare the file loading
+      // reader.readAsDataURL(file);
+      // setTimeout(test, 2000);
+      
+      // }
+
+      
+
+
+      const croppie = new Croppie(document.querySelector('#image1'), {
           viewport: {
                 width: 150,
                 height: 200
@@ -107,7 +163,7 @@
       fixImage.addEventListener('click', function(){
         //alert("work");
         alert(croppie.getCroppedCanvas().toDataURL("image/png"));
-        var croppieImage = croppie.get().toDataURL("image/png");
+        var croppieImage = croppie.getCroppedCanvas().toDataURL("image/png");
 
         alert(croppieImage);
 
@@ -116,45 +172,6 @@
         //upload1.src = croppieImage;
 
       });
-        
-      }
-      function myFunction() {
-
-        // croppie.destroy();
-        // croppie = 0;
-      
-      let boxImage1 = document.querySelector('#boxImage1');
-      boxImage1.style = "width: 100%;height: 400px;margin-bottom: 50px;";
-      var file = document.getElementById('file').files[0];
-      var reader  = new FileReader();
-      // it's onload event and you forgot (parameters)
-      reader.onload = function(e)  {
-          //var image = document.createElement("img");
-          image = document.querySelector('#image1');
-          // the result image data
-          image.src = e.target.result;
-          //document.body.appendChild(image);
-      }
-      // you have to declare the file loading
-      reader.readAsDataURL(file);
-      setTimeout(test, 2000);
-      
-      }
-
-      // let fixImage = document.querySelector('#fixImage');
-
-      
-      // fixImage.addEventListener('click', function(){
-      //   alert("work");
-      //   var croppieImage = croppie.getCroppedCanvas().toDataURL("image/png");
-
-      //   alert(croppieImage);
-
-      //   //let upload1 = document.querySelector('#upload1');
-
-      //   //upload1.src = croppieImage;
-
-      // });
 
        
      
