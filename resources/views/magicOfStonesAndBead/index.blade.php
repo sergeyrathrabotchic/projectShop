38a69ad976ae819fb0e1b74ba93ae7d37e8b496e
Sergey <skilinlive@mail.ru>
2023-04-24 17:03:01 +0300

    fix

--- resources/views/magicOfStonesAndBead/index.blade.php
+++ resources/views/magicOfStonesAndBead/index.blade.php
@@ -64,10 +64,10 @@
         <div>
           <div style="display: flex;justify-content: center;">
             <a   href="https://wa.me/79631450453" target="_blank" style="text-decoration: none;color: black;display: flex;">
-              <img style="width: 35px;height: 35px;margin-right: 5px;" alt="Logo" src="/assets/media/staticImages/whatsapp2.png" >
+              <img style="width: 35px;height: 35px;margin-right: 5px;padding: 5px" alt="Logo" src="/assets/media/staticImages/whatsapp2.png" >
             </a>
             <a   href="https://wa.me/79631450453" target="_blank" style="text-decoration: none;color: black;display: flex;">
-              <img style="width: 35px;height: 35px;margin-right: 5px;" alt="Logo" src="/assets/media/staticImages/vk.png" >
+              <img style="width: 35px;height: 35px;margin-right: 5px;padding: 6px;" alt="Logo" src="/assets/media/staticImages/vk.png" >
             </a>
           </div>
         </div>
