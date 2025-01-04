0d393f1b92dce0ec5c985ca120824d78b18dcea4
Sergey <skilinlive@mail.ru>
2025-01-04 17:45:13 +0300

    fix

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -19,7 +19,7 @@ class RouteServiceProvider extends ServiceProvider
      */
     public const HOME = '/home';
 
-    public const ACCOUNT = '/account'
+    public const ACCOUNT = '/account';
 
     /**
      * The controller namespace for the application.
