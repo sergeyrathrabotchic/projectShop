64a51d07449852c10caa24b4c0f3503cf26d7376
Sergey <skilinlive@mail.ru>
2025-01-04 17:22:02 +0300

    добавили отступы

--- resources/views/account/index.blade.php
+++ resources/views/account/index.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.app')
 @section('content')
-    <h2>Привет {{Auth::user()->name}}</h2>
-    <br>
-    <a href="{{route('admin.index')}}">Перейти в админку</a>
+    <div class="offset-2">
+        <h2>Привет {{Auth::user()->name}}</h2>
+        <br>
+        <a href="{{route('admin.index')}}">Перейти в админку</a>
+    </div>
+
 @endsection
