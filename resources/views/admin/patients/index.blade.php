b7b5cb43706cca8c12ec8da70731daa3169611d6
Sergey <skilinlive@mail.ru>
2025-02-08 22:44:05 +0300

    создание

--- resources/views/admin/patients/index.blade.php
+++ resources/views/admin/patients/index.blade.php
@@ -1,1 +1,6 @@
-hello
+@extends('layosts.admin')
+@section('title') Список пациентов - @parent @stop
+@section('content')
+<div class="btn-group me-2">
+    <a href="{{route('admin.patients.create')}}" class="btn btn-sm btn-outline-secondary">Добавить нового поциента</a>
+</div>
