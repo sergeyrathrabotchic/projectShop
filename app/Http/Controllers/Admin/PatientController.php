402814587495930e15ee7d27a42ae1b6096aad04
Sergey <skilinlive@mail.ru>
2025-02-09 01:05:18 +0300

    создание

--- app/Http/Controllers/Admin/PatientController.php
+++ app/Http/Controllers/Admin/PatientController.php
@@ -19,6 +19,7 @@ class PatientController extends Controller
     public function index()
     {
         $patients = Patient::paginate(5);
+        dd(gettype($patients[0]->created_at));
         dd(gettype($patients[0]->birthdate));
         return view('admin.patients.index', [
             'patients' => $patients
