c91640471e4b8c75a0012e47c1d70f705f67ea98
Sergey <skilinlive@mail.ru>
2025-02-09 20:45:53 +0300

    создание

--- app/Helpers/PatientCreateHelper.php
+++ app/Helpers/PatientCreateHelper.php
@@ -9,11 +9,11 @@ class PatientCreateHelper
     public static function patientCreate($patient)
     {
         $patient = Patient::create([
-            'first_name' => $patient->first_name,
-            'last_name' => $patient->last_name,
-            'birthdate' => $patient->birthdate,
-            'age' => $patient->age,
-            'age_type' => $patient->age_type,
+            'first_name' => $patient['first_name'],
+            'last_name' => $patient['last_name'],
+            'birthdate' => $patient['birthdate'],
+            'age' => $patient['age'],
+            'age_type' => $patient['age_type'],
         ]);
 
         // return $patient;
