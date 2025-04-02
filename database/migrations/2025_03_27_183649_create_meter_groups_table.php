48ce98b897522795fa7df869f0bc9f49b12d6505
Sergey <skilinlive@mail.ru>
2025-04-02 23:48:28 +0300

    поправил таблицы

--- database/migrations/2025_03_27_183649_create_meter_groups_table.php
+++ database/migrations/2025_03_27_183649_create_meter_groups_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateMeterGroupTable extends Migration
+class CreateMeterGroupsTable extends Migration
 {
     /**
      * Run the migrations.
--- database/migrations/2025_03_29_143850_create_personals_table.php
+++ database/migrations/2025_03_29_143850_create_personals_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePersonalTable extends Migration
+class CreatePersonalsTable extends Migration
 {
     /**
      * Run the migrations.
