2cbe14c62d51a8419f9246c714b89b93266e7441
Sergey <skilinlive@mail.ru>
2025-04-13 00:21:11 +0300

    поправил меню админки

--- resources/views/admin/tarifs/edit.blade.php
+++ resources/views/admin/tarifs/edit.blade.php
@@ -34,7 +34,7 @@
               <input type="test" style="margin: 4px;width:98%;" class="form-control" name="title" id="title" @if ($tarif->title) value="{{$tarif->title}}" @endif>
             <div class="form-group">
               <label for="image">Цена</label>
-              <input type="test" style="margin: 4px;width:98%;" class="form-control" name="price" id="price" @if ($tarif->pricee) value="{{$tarif->price}}" @endif>
+              <input type="test" style="margin: 4px;width:98%;" class="form-control" name="price" id="price" @if ($tarif->price) value="{{$tarif->price}}" @endif>
             </div>
            
             <br>
