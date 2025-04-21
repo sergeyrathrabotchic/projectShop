074b047c6a750f6d5ba0c5b28cb1e4d1ab658108
Sergey <skilinlive@mail.ru>
2025-04-22 00:10:54 +0300

    js сделал классы

--- resources/views/admin/waterWithdrawals/index.blade.php
+++ resources/views/admin/waterWithdrawals/index.blade.php
@@ -171,9 +171,8 @@
             }
           }
           var pump_1_condition = []
-          for(i=0;i<pump_1.length;i++){
-            pump_1[i].addEventListener('click', function (pump_1 = pump_1[i]){
-              if (pump_1.innerHTML == "Остановить") {
+          function pump_1_addEventListener(pump_1,pump_1_condition){
+            if (pump_1.innerHTML == "Остановить") {
                 pump_1.innerHTML = "Запустить" 
                 pump_1_condition.push(0);
                 pump_1.classList.remove('costumeChange', 'btn-danger');
@@ -184,7 +183,9 @@
                 pump_1.classList.remove('costumeChange', 'btn-success');
                 pump_1.classList.add('btn-danger')
               }
-            });
+          }
+          for(i=0;i<pump_1.length;i++){
+            pump_1[i].addEventListener('click', pump_1_addEventListener, pump_1[i],pump_1_condition);
           }
           // var pump_1 = document.getElementById("pump_1");
           // var waterWithdrawals_1_value = document.querySelector('#waterWithdrawals_1_value');
