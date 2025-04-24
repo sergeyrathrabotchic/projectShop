4131bf4d571ce6ddc3d9e9dc5f3e0750c381f820
Sergey <skilinlive@mail.ru>
2025-04-24 22:47:53 +0300

    сделал добавление насосов

--- resources/views/admin/waterWithdrawals/index.blade.php
+++ resources/views/admin/waterWithdrawals/index.blade.php
@@ -48,7 +48,7 @@
                   <td>
                     {{-- <h6>{{$address->street}}</h6> --}}
                     <div class="form-group" style="display: flex;flex-direction: column;">
-                     {{-- @forelse ($reservoirs->pump as $pump)
+                     @forelse ($reservoirs->pump as $pump)
                          @if ($pump->id_reservoir == 0)
                           @php
                               $k = $k +1;
@@ -62,7 +62,7 @@
                         </div>
                         @endif
                       @empty
-                      @endforelse --}}
+                      @endforelse
                       <div style="display: flex;">
                         <label for="image">Насос №2 (емк. 1)</label>
                         <div style="display: flex;">
@@ -90,7 +90,7 @@
                       </div>
                     </div>
                     <div class="form-group" style="display: flex;flex-direction: column;">
-                      {{-- @forelse ($reservoirs->pump as $pump)
+                      @forelse ($reservoirs->pump as $pump)
                         @if ($pump->id_reservoir == 1)
                           @php
                               $j = $j +1;
@@ -104,7 +104,7 @@
                         </div>
                         @endif
                       @empty
-                      @endforelse --}}
+                      @endforelse
                       <div style="display: flex;">
                         <label for="image">Насос №1 (емк. 2)</label>
                         <div style="display: flex;">
