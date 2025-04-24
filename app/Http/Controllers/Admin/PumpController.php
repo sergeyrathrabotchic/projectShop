dc47e8fd1ac22b1e04e058f63a69cfd6443afeb1
Sergey <skilinlive@mail.ru>
2025-04-24 23:56:23 +0300

    сделал добавление насосов

--- app/Http/Controllers/Admin/PumpController.php
+++ app/Http/Controllers/Admin/PumpController.php
@@ -83,7 +83,8 @@ class PumpController extends Controller
     public function edit(Pump $pump)
     {
        // $address = $address::with('meterGroup.meter');
-       $pump = Pump::where('id', '=', $pump->id_group)->get();
+       $pump = Pump::where('id', '=', $pump->id)->get();
+
        return view('admin.pumps.edit', [
            'pump' => $pump,
        ]);
