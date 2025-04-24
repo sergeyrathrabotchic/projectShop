4565604de40c0ba173628b7884c744566a9f91c1
Sergey <skilinlive@mail.ru>
2025-04-24 22:17:05 +0300

    сделал добавление насосов

--- app/Http/Controllers/Admin/WaterWithdrawalController.php
+++ app/Http/Controllers/Admin/WaterWithdrawalController.php
@@ -18,7 +18,8 @@ class WaterWithdrawalController extends Controller
     public function index(Request $request)
     {
         // $addresses =  Address::with('meterGroup.meter')->paginate(5);
-        $reservoirs =  Reservoir::with('pump')->paginate(5);
+        $reservoirs =  Reservoir::with('pump')->paginate(2);
+        dd($reservoirs);
         $page = $request->get('page', 1);
         if ($page > 0) {
             $page = ($page - 1) * 5;
