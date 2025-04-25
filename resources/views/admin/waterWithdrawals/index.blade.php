106ccd77bfb06a83a1d4d41812289bfe7c7f3d25
Sergey <skilinlive@mail.ru>
2025-04-25 19:27:42 +0300

    сделал добавление насосов

--- resources/views/admin/waterWithdrawals/index.blade.php
+++ resources/views/admin/waterWithdrawals/index.blade.php
@@ -340,7 +340,7 @@
               up_pump_emk1.style = "";
               dovn_pump_emk1.style = "display: none;";
             } else {
-              let sum =0;
+              let sum = parseInt(barChart.data.datasets[0].data[0]);
               for(i = 1;sum < 0;i++){
                 sum = parseInt(barChart.data.datasets[0].data[0]) + emk1 * i;
               } 
