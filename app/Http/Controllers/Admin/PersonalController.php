98d55b5a2dff0f084e59017a2e274a2c59797641
Sergey <skilinlive@mail.ru>
2025-04-29 21:06:11 +0300

    правка

--- app/Http/Controllers/Admin/PersonalController.php
+++ app/Http/Controllers/Admin/PersonalController.php
@@ -93,6 +93,7 @@ class PersonalController extends Controller
         $account = Account::where('id', '=', $personal->id_account)->get();
         // dd($account);
         $addresId = Address::where('id', '=', $account[0]->id)->get();
+        dd($addresId);
         $addresses = Address::all();
         return view('admin.personals.edit', [
             'personal' => $personal,
