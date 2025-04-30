e34ab7f5315c2e4a941539b5c610d288969f43ae
Sergey <skilinlive@mail.ru>
2025-04-30 20:07:14 +0300

    правка

--- app/Http/Controllers/Admin/ChargeController.php
+++ app/Http/Controllers/Admin/ChargeController.php
@@ -19,7 +19,7 @@ class ChargeController extends Controller
     public function index(Account $account, Request $request)
     {
         // $accounts =  Account::with(['address.meterGroup.meter','personal', 'charge'])->where("id", "=", $account->id)->paginate(5);
-        $accounts =  Account::where("id", "=", $account->id);
+        $accounts =  Account::where("id", "=", $account->id)->get();
         dd($accounts);
         $page = $request->get('page', 1);
         if ($page > 0) {
