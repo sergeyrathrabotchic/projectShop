a51ea311a2e00e495e54f0beae925928bee3dfb9
Sergey <skilinlive@mail.ru>
2025-04-30 23:48:26 +0300

    правка

--- resources/views/admin/charges/index.blade.php
+++ resources/views/admin/charges/index.blade.php
@@ -2,7 +2,8 @@
 @section('title') Начисление - @parent @stop
 @section('content')
 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-    <h1 class="h2">Начисление {{$account[0]->personal->sub_addr}} {{$account[0]->personal->FIO}}</h1>
+   {{$account[0]->personal}} 
+  <h1 class="h2">Начисление {{$account[0]->personal->sub_addr}} {{$account[0]->personal->FIO}}</h1>
     <div class="btn-toolbar mb-2 mb-md-0">
       <div class="btn-group me-2">
         <a href="{{route('admin.personals.create')}}" class="btn btn-sm btn-outline-secondary">Добавить новое физическое лицо</a>
