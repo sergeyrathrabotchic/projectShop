50c1bd242b2a2429ef2b581c13c0c043ffece21d
Sergey <skilinlive@mail.ru>
2025-05-08 15:50:32 +0300

    правка

--- resources/views/admin/overpayments/index.blade.php
+++ resources/views/admin/overpayments/index.blade.php
@@ -60,8 +60,8 @@
                   <td>{{$i}}</td>
                   {{-- {{dd($account->personal->where('id_account','=',$account->id))}} --}}
                   <td>
-                    @if ($account->personal->where('id_account','=',$account->id)[0])
-                    {{dd($account->personal->where('id_account','=',$account->id))}}
+                    @if ($account->personal->where('id_account','=',$account->id)[0]->isNotEmpty())
+                    {{-- {{dd($account->personal->where('id_account','=',$account->id))}} --}}
                       <h6>{{$account->personal->where('id_account','=',$account->id)[0]->sub_addr}} {{$account->personal->where('id_account','=',$account->id)[0]->FIO}}</h6>
                     @else
                     {{-- {{dd($account->org->where('id_account','=',$account->id))}} --}}
